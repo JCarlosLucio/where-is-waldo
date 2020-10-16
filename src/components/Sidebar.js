d725dd6ec68df16a99170987dc156f8abf482497
jcLucio <c.lucio3@gmail.com>
2020-10-16 18:40:00 -0500

    Add Sidebar file w/boilerplate

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div>
+      <h1>Where is Waldo?</h1>
+      {/* SearchLists go here */}
+    </div>
+  );
+}
+
+export default Sidebar;
