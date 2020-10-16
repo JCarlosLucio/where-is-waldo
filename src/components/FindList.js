2b3be8fe2d49bd34d4489d7895d63118930ab90d
jcLucio <c.lucio3@gmail.com>
2020-10-16 18:51:59 -0500

    Add FindItem component

--- src/components/FindList.js
+++ src/components/FindList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FindItem from './FindItem';
 
 function FindList() {
   return (
     <div>
       <h1>Difficulty</h1>
       {/* FindItems go here */}
+      <FindItem />
     </div>
   );
 }
