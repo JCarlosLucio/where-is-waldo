abe98d81176fd76d026cc12e455b9819e28e384a
jcLucio <c.lucio3@gmail.com>
2020-10-16 18:45:38 -0500

    Add FindItem file w/boilerplate

---
+++ src/components/FindItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FindItem() {
+  return (
+    <div>
+      {/* character image goes here */}
+      <h2>Character Name</h2>
+      <h5>Character Franchise</h5>
+    </div>
+  );
+}
+
+export default FindItem;
