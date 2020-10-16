3db4a8e9c25ec6ca571a9801c6fc641043ce0344
jcLucio <c.lucio3@gmail.com>
2020-10-16 18:45:58 -0500

    Add FindList file w/boilerplate

---
+++ src/components/FindList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FindList() {
+  return (
+    <div>
+      <h1>Difficulty</h1>
+      {/* FindItems go here */}
+    </div>
+  );
+}
+
+export default FindList;
