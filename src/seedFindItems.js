d7c70786fb4832a8b6dc3e57768e2b707ae7abd2
jcLucio <c.lucio3@gmail.com>
2020-10-16 19:04:00 -0500

    Add initial seedFindItem

---
+++ src/seedFindItems.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 'easy-list',
+    difficulty: 'easy',
+    items: [
+      {
+        id: 'character-item',
+        name: 'Character 1',
+        franchise: 'Chacter Franchise',
+        found: false,
+      },
+    ],
+  },
+];
