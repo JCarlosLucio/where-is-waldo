5751a413ae5e3a07681e1defa8496cb1bfe6bf7f
jcLucio <c.lucio3@gmail.com>
2020-10-16 22:04:26 -0500

    Add GameImage component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
+import GameImage from './components/GameImage';
 import seedFindItems from './seedFindItems';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Sidebar findLists={lists} />
       {/* where is waldo type image goes here */}
+      <GameImage />
     </div>
   );
 }
