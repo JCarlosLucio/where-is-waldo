b7f4c91e41de6c35d4034e5dc648ca59e347e45f
jcLucio <c.lucio3@gmail.com>
2020-10-16 22:02:31 -0500

    Refactor seedFindItems to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
+import seedFindItems from './seedFindItems';
 
 function App() {
+  const [lists, setLists] = useState(seedFindItems);
+
   return (
     <div>
-      <Sidebar />
+      <Sidebar findLists={lists} />
       {/* where is waldo type image goes here */}
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import FindList from './FindList';
-import seedFindItems from '../seedFindItems';
 
-function Sidebar() {
+function Sidebar({ findLists }) {
   return (
     <div>
       <h1>Where is Waldo?</h1>
-      {seedFindItems.map((list) => <FindList {...list} key={list.id} />)}
+      {findLists.map((list) => <FindList {...list} key={list.id} />)}
     </div>
   );
 }
