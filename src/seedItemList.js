b326b15852fe0d9c53f4252b3ebdfcb51a2a7993
jcLucio <c.lucio3@gmail.com>
2020-10-23 18:07:40 -0500

    Refactor seedItemList data structure and name

---
+++ src/seedItemList.js
@@ -0,0 +1,11 @@
+const seedItemList = [
+  {
+    id: 'character-item',
+    name: 'Character 1',
+    franchise: 'Character Franchise',
+    difficulty: 'easy',
+    found: false,
+  },
+];
+
+export default seedItemList;
