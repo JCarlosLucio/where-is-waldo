e8b5ffd9d23c61254437ceacbadc44f54ac83acf
jcLucio <c.lucio3@gmail.com>
2020-10-23 19:11:13 -0500

    Add image src and comments w/attribution for later

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -4,6 +4,11 @@ function GameImage() {
   return (
     <div>
       <h1>GAME IMAGE</h1>
+      <img src="https://i.imgur.com/N4wefbX.jpg" alt="test" />
+      {/* The Loc Nar by Egor Klyuchnyk*/}
+      {/* Instagram https://www.instagram.com/ad.2.222/*/}
+      {/* Patreon https://www.patreon.com/Egor*/}
+      {/* Buy his poster https://anomaly-world.com/posters-by-anomaly-world/cyberpunk-poster-ad2222*/}
     </div>
   );
 }
