45fa1e7b0326e03111bf818bc076bdc694ba3274
jcLucio <c.lucio3@gmail.com>
2020-10-23 19:25:00 -0500

    Update map constant

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -3,7 +3,7 @@ import ImageMapper from 'react-image-mapper';
 
 function GameImage() {
   const IMAGE_URL = 'https://i.imgur.com/N4wefbX.jpg';
-  const MAP = {
+  const AREAS_MAP = {
     name: 'The Loc Nar',
     areas: [],
   };
@@ -11,7 +11,7 @@ function GameImage() {
   return (
     <div>
       <h1>GAME IMAGE</h1>
-      <ImageMapper src={IMAGE_URL} map={MAP} />
+      <ImageMapper src={IMAGE_URL} map={AREAS_MAP} />
       {/* The Loc Nar by Egor Klyuchnyk*/}
       {/* Instagram https://www.instagram.com/ad.2.222/*/}
       {/* Patreon https://www.patreon.com/Egor*/}
