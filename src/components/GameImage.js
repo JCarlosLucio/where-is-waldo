1470a3527227e60953eddf93d9c72d188f6c9220
jcLucio <c.lucio3@gmail.com>
2020-10-23 19:21:30 -0500

    Add ImageMapper

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ImageMapper from 'react-image-mapper';
 
 function GameImage() {
   return (
     <div>
       <h1>GAME IMAGE</h1>
-      <img src="https://i.imgur.com/N4wefbX.jpg" alt="test" />
+      <ImageMapper src={'https://i.imgur.com/N4wefbX.jpg'} alt="test" />
       {/* The Loc Nar by Egor Klyuchnyk*/}
       {/* Instagram https://www.instagram.com/ad.2.222/*/}
       {/* Patreon https://www.patreon.com/Egor*/}
