569d9e6622e365f7a2b1602813136c7de90436f2
jcLucio <c.lucio3@gmail.com>
2020-10-23 19:23:58 -0500

    Add initial map

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -3,11 +3,15 @@ import ImageMapper from 'react-image-mapper';
 
 function GameImage() {
   const IMAGE_URL = 'https://i.imgur.com/N4wefbX.jpg';
+  const MAP = {
+    name: 'The Loc Nar',
+    areas: [],
+  };
 
   return (
     <div>
       <h1>GAME IMAGE</h1>
-      <ImageMapper src={IMAGE_URL} />
+      <ImageMapper src={IMAGE_URL} map={MAP} />
       {/* The Loc Nar by Egor Klyuchnyk*/}
       {/* Instagram https://www.instagram.com/ad.2.222/*/}
       {/* Patreon https://www.patreon.com/Egor*/}
