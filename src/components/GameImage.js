d25d15b9a561b011b5bd97b878687bcf151ba219
jcLucio <c.lucio3@gmail.com>
2020-10-24 20:03:20 -0500

    Remove ImageMapper

--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ImageMapper from 'react-image-mapper';
 
 function GameImage() {
   const IMAGE_URL = 'https://i.imgur.com/N4wefbX.jpg';
-  const AREAS_MAP = {
-    name: 'The Loc Nar',
-    areas: [],
-  };
 
   return (
     <div>
       <h1>GAME IMAGE</h1>
-      <ImageMapper src={IMAGE_URL} map={AREAS_MAP} />
       {/* The Loc Nar by Egor Klyuchnyk*/}
       {/* Instagram https://www.instagram.com/ad.2.222/*/}
       {/* Patreon https://www.patreon.com/Egor*/}
