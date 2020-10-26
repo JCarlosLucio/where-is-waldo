c0e50e08d00781f8ebdcd75909ca1a7a2598ff54
jcLucio <c.lucio3@gmail.com>
2020-10-25 20:33:34 -0600

    Add difficulty styles

--- src/components/FindList.js
+++ src/components/FindList.js
@@ -5,7 +5,7 @@ import styles from './FindList.module.scss';
 function FindList({ difficulty, items }) {
   return (
     <div className={styles.root}>
-      <h2>{difficulty}</h2>
+      <h2 className={styles[difficulty]}>{difficulty}</h2>
       {items.map((item) => <FindItem {...item} key={item.id} />)}
     </div>
   );
