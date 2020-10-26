22b03fdd91a5783f13f94e99104712329f4deddd
jcLucio <c.lucio3@gmail.com>
2020-10-25 20:31:11 -0600

    Add FindList styles

--- src/components/FindList.js
+++ src/components/FindList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import FindItem from './FindItem';
+import styles from './FindList.module.scss';
 
 function FindList({ difficulty, items }) {
   return (
-    <div>
+    <div className={styles.root}>
       <h2>{difficulty}</h2>
       {items.map((item) => <FindItem {...item} key={item.id} />)}
     </div>
