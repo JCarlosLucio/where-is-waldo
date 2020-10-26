2bcbe1629db29b0b29ba2736a401420b2c5bd0f9
jcLucio <c.lucio3@gmail.com>
2020-10-26 16:30:12 -0600

    Add img element

--- src/components/FindItem.js
+++ src/components/FindItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function FindItem({ name, franchise }) {
+function FindItem({ name, franchise, image }) {
   return (
     <div>
       {/* character image goes here */}
       {/* conditional style w/found prop */}
+      <img src={image} alt={name} />
       <h3>{name}</h3>
       <h5>{franchise}</h5>
     </div>
