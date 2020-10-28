db44463cafbd8e73448386b536b7d5bd1fef9168
jcLucio <c.lucio3@gmail.com>
2020-10-28 16:20:30 -0600

    Add DropdownMenu file w/boilerplate

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function DropdownMenu() {
+  return (
+    <div>
+      Dropdown Menu
+      {/* Move to Dropdown menu */}
+      {/* <FindList
+        difficulty="easy"
+        items={list.filter((item) => item.difficulty === 'easy')}
+      />
+      <FindList
+        difficulty="medium"
+        items={list.filter((item) => item.difficulty === 'medium')}
+      />
+      <FindList
+        difficulty="hard"
+        items={list.filter((item) => item.difficulty === 'hard')}
+      /> */}
+    </div>
+  );
+}
+
+export default DropdownMenu;
