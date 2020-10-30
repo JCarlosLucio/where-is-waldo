470b89179ad9ced3b4b49894297121668e7c26b9
jcLucio <c.lucio3@gmail.com>
2020-10-30 12:12:15 -0600

    Add StartDialog file w/boilerplate

---
+++ src/components/StartDialog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function StartDialog() {
+  return (
+    <div>
+      <h1>findUS</h1>
+      {/* Image preview (?) */}
+      <p>List of Items to Find</p>
+      <button>START</button>
+    </div>
+  );
+}
+
+export default StartDialog;
