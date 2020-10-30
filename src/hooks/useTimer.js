f523372da74afa20e2c2d163c9a95b4359fd41f5
jcLucio <c.lucio3@gmail.com>
2020-10-29 22:30:52 -0600

    Add useTimer custom hook

---
+++ src/hooks/useTimer.js
@@ -0,0 +1,22 @@
+import { useRef, useState } from 'react';
+
+function useTimer(initialVal = 0) {
+  const [timer, setTimer] = useState(initialVal);
+  const countRef = useRef(null);
+
+  // adds 1 to timer every second
+  const start = () => {
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+  // stops and resets timer
+  const stop = () => {
+    clearInterval(countRef.current);
+    setTimer(0);
+  };
+
+  return [timer, start, stop];
+}
+
+export default useTimer;
