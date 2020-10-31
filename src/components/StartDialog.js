1f0c96bad115fabacc2e021b6cec0160fb60a3ca
jcLucio <c.lucio3@gmail.com>
2020-10-31 17:51:08 -0600

    Add div for info

--- src/components/StartDialog.js
+++ src/components/StartDialog.js
@@ -6,21 +6,23 @@ import styles from './StartDialog.module.scss';
 function StartDialog({ list, imageUrl, handleStart }) {
   return (
     <div className={styles.root}>
-      <Logo big primary="black" secondary="red" />
       <img src={imageUrl} alt="the loc nar" />
-      <FindList
-        difficulty="easy"
-        items={list.filter((item) => item.difficulty === 'easy')}
-      />
-      <FindList
-        difficulty="medium"
-        items={list.filter((item) => item.difficulty === 'medium')}
-      />
-      <FindList
-        difficulty="hard"
-        items={list.filter((item) => item.difficulty === 'hard')}
-      />
-      <button onClick={handleStart}>START</button>
+      <div className={styles.info}>
+        <Logo big primary="black" secondary="red" />
+        <FindList
+          difficulty="easy"
+          items={list.filter((item) => item.difficulty === 'easy')}
+        />
+        <FindList
+          difficulty="medium"
+          items={list.filter((item) => item.difficulty === 'medium')}
+        />
+        <FindList
+          difficulty="hard"
+          items={list.filter((item) => item.difficulty === 'hard')}
+        />
+        <button onClick={handleStart}>START</button>
+      </div>
     </div>
   );
 }
