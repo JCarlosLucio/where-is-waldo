537464fd9562055bbfb004046561db0dc1f7f3be
jcLucio <c.lucio3@gmail.com>
2020-10-30 21:04:07 -0600

    Add StartDialog styles

--- src/components/StartDialog.js
+++ src/components/StartDialog.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './StartDialog.module.scss';
 
 function StartDialog() {
   return (
-    <div>
+    <div className={styles.root}>
       <h1>findUS</h1>
       {/* Image preview (?) */}
       <p>List of Items to Find</p>
