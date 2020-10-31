dd2c190a3c7625e5f1d9bf59e944f5a7b8492501
jcLucio <c.lucio3@gmail.com>
2020-10-30 22:12:25 -0600

    Add Logo styles

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from './Logo.module.scss';
 
-function Logo() {
-  return <h1>findUs</h1>;
+function Logo({ big, primary, secondary }) {
+  return (
+    <h1 className={big ? styles.big : styles.root}>
+      <span style={{ color: primary }}>find</span>
+      <span style={{ color: secondary }}>Us</span>
+    </h1>
+  );
 }
 
 export default Logo;
