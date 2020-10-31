a6df209e1a37bafaeb36dcdec076a8a0909636e2
jcLucio <c.lucio3@gmail.com>
2020-10-30 21:49:21 -0600

    Shorten timer format

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,5 +4,5 @@ export const formatTime = (time) => {
   const getMinutes = `0${minutes % 60}`.slice(-2);
   const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
 
-  return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  return `${getHours}:${getMinutes}:${getSeconds}`;
 };
