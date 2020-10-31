2ba486274071a5cbda87c3c8b875874447983ffd
jcLucio <c.lucio3@gmail.com>
2020-10-30 22:13:45 -0600

    Refactor title w/ Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useToggle from '../hooks/useToggle';
 import DropdownMenu from './DropdownMenu';
+import Logo from './Logo';
 import { formatTime } from '../utils';
 import styles from './Navbar.module.scss';
 
@@ -12,7 +13,9 @@ function Navbar({ list, timer }) {
   return (
     <nav className={styles.root}>
       <ul className={styles.nav}>
-        <li className={styles.title}>Where is Waldo?</li>
+        <li>
+          <Logo primary="white" secondary="red" />
+        </li>
         <li className={styles.timer}>{formatTime(timer)}</li>
         <li className={styles.dropdown}>
           <button className={styles['dropdown-btn']} onClick={toggleOpen}>
