25f0e31b6d4054947b7a6db7aeecfdaed532a462
jcLucio <c.lucio3@gmail.com>
2020-11-01 13:39:11 -0600

    Add StartDialog to Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import StartDialog from './StartDialog';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-function Carousel() {
-  return <div>IM Carousel</div>;
+const responsive = {
+  0: { items: 1 },
+  568: { items: 1 },
+  1024: { items: 1 },
+};
+
+function Carousel({ imageList, handleStart }) {
+  const images = imageList.map((image) => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <StartDialog
+        list={image.itemList}
+        imageName={image.imageName}
+        imageUrl={image.imageUrl}
+        handleStart={handleStart}
+      />
+    </div>
+  ));
+  console.log(images);
+  return <AliceCarousel mouseTracking items={images} responsive={responsive} />;
 }
 
 export default Carousel;
