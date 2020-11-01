c746459a180ef2a4a3f9e558a964df6f02d453ea
jcLucio <c.lucio3@gmail.com>
2020-11-01 13:37:24 -0600

    Add second image

--- src/seedImageList.js
+++ src/seedImageList.js
@@ -36,6 +36,43 @@ const imageList = [
       },
     ],
   },
+  {
+    id: 'second-image',
+    imageName: 'Second Image',
+    imageUrl: 'https://i.imgur.com/N4wefbX.jpg',
+    itemList: [
+      // Easy
+      {
+        id: 'bowser',
+        name: 'Bowser',
+        franchise: 'Super Mario Bros',
+        image:
+          'https://upload.wikimedia.org/wikipedia/en/1/11/BowserNSMBUD.png',
+        difficulty: 'easy',
+        found: false,
+      },
+      // Medium
+      {
+        id: 'yubaba',
+        name: 'Yubaba',
+        franchise: 'Spirited Away',
+        image:
+          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e23b6aa5-49a1-432b-8b94-685797a9d094/d5mhc3y-976ec73d-0c23-4802-a369-b5ba2e930651.png/v1/fill/w_400,h_386,strp/yubaba_by_rodupanda_d5mhc3y-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0zODYiLCJwYXRoIjoiXC9mXC9lMjNiNmFhNS00OWExLTQzMmItOGI5NC02ODU3OTdhOWQwOTRcL2Q1bWhjM3ktOTc2ZWM3M2QtMGMyMy00ODAyLWEzNjktYjViYTJlOTMwNjUxLnBuZyIsIndpZHRoIjoiPD00MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.2UMI8Q4jGysU-_2wLIkIcqvlqMzywKxDXHZO2jQi26o',
+        difficulty: 'medium',
+        found: false,
+      },
+      // Hard
+      {
+        id: 'the-knight',
+        name: 'The Knight',
+        franchise: 'Hollow Knight',
+        image:
+          'https://static.wikia.nocookie.net/jerma-lore/images/8/81/Hollowknight.png',
+        difficulty: 'hard',
+        found: false,
+      },
+    ],
+  },
 ];
 
 export default imageList;
