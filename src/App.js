298b13b0c0749685be70d903675458777cc3aaa7
jcLucio <c.lucio3@gmail.com>
2020-10-31 19:49:05 -0600

    Refactor App to use gameImage from seedImageList

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Navbar from './components/Navbar';
 import GameImage from './components/GameImage';
 import Modal from './components/Modal';
 import StartDialog from './components/StartDialog';
-import seedItemList from './seedItemList';
+import seedImageList from './seedImageList';
 import styles from './App.module.scss';
 
 const IMAGE_URL = 'https://i.imgur.com/N4wefbX.jpg';
 
 function App() {
-  // list of items to find
-  const [list, setList] = useState(seedItemList);
+  // image for game w/ list of items to find
+  const [gameImage, setGameImage] = useState(seedImageList[0]);
   // timer for how long it takes to find items
   const [timer, start, stop] = useTimer(0);
   // state for opening Modal
@@ -23,26 +23,26 @@ function App() {
     start();
   };
 
-  const toggleFound = (id) => {
-    const updatedItems = list.map((item) => {
-      if (item.id === id) {
-        return { ...item, found: true };
-      }
-      return item;
-    });
-    setList(updatedItems);
-  };
+  // const toggleFound = (id) => {
+  //   const updatedItems = list.map((item) => {
+  //     if (item.id === id) {
+  //       return { ...item, found: true };
+  //     }
+  //     return item;
+  //   });
+  //   setList(updatedItems);
+  // };
 
   return (
     <div className={styles.root}>
-      <Navbar list={list} timer={timer} />
-      <GameImage toggleFound={toggleFound} imageUrl={IMAGE_URL} />
+      <Navbar list={gameImage.itemList} timer={timer} />
+      <GameImage toggleFound={toggleFound} imageUrl={gameImage.imageUrl} />
       {/* modal for starting game/timer */}
       {modalOpen && (
         <Modal>
           <StartDialog
-            list={list}
-            imageUrl={IMAGE_URL}
+            list={gameImage.itemList}
+            imageUrl={gameImage.imageUrl}
             handleStart={handleStart}
           />
         </Modal>
