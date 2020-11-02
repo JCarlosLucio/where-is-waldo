14f6bd492ec73120a607a463c3eda902a03f1e2a
jcLucio <c.lucio3@gmail.com>
2020-11-01 19:15:13 -0600

    Update images for itemsLists

--- src/seedImageList.js
+++ src/seedImageList.js
@@ -1,7 +1,13 @@
+// The Loc Nar
 import theLocNar from './assets/the-loc-nar.jpg';
+import bowser from './assets/bowser.png';
+import yubaba from './assets/yubaba.png';
+import theKnight from './assets/the-knight.webp';
+// Universe 113
 import universe113 from './assets/universe-113.jpg';
-import courage from './assets/courage.png';
+import cacodemon from './assets/cacodemon.png';
 import billCipher from './assets/bill-cipher.png';
+import courage from './assets/courage.png';
 
 const imageList = [
   {
@@ -20,8 +26,7 @@ const imageList = [
         id: 'bowser',
         name: 'Bowser',
         franchise: 'Super Mario Bros',
-        image:
-          'https://upload.wikimedia.org/wikipedia/en/1/11/BowserNSMBUD.png',
+        image: bowser,
         difficulty: 'easy',
         found: false,
       },
@@ -30,8 +35,7 @@ const imageList = [
         id: 'yubaba',
         name: 'Yubaba',
         franchise: 'Spirited Away',
-        image:
-          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e23b6aa5-49a1-432b-8b94-685797a9d094/d5mhc3y-976ec73d-0c23-4802-a369-b5ba2e930651.png/v1/fill/w_400,h_386,strp/yubaba_by_rodupanda_d5mhc3y-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0zODYiLCJwYXRoIjoiXC9mXC9lMjNiNmFhNS00OWExLTQzMmItOGI5NC02ODU3OTdhOWQwOTRcL2Q1bWhjM3ktOTc2ZWM3M2QtMGMyMy00ODAyLWEzNjktYjViYTJlOTMwNjUxLnBuZyIsIndpZHRoIjoiPD00MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.2UMI8Q4jGysU-_2wLIkIcqvlqMzywKxDXHZO2jQi26o',
+        image: yubaba,
         difficulty: 'medium',
         found: false,
       },
@@ -40,8 +44,7 @@ const imageList = [
         id: 'the-knight',
         name: 'The Knight',
         franchise: 'Hollow Knight',
-        image:
-          'https://static.wikia.nocookie.net/jerma-lore/images/8/81/Hollowknight.png',
+        image: theKnight,
         difficulty: 'hard',
         found: false,
       },
@@ -63,7 +66,7 @@ const imageList = [
         id: 'cacodemon',
         name: 'Cacodemon',
         franchise: 'DOOM',
-        image: 'https://doomwiki.org/w/images/d/dd/Codex_cacodemon.bimage.png',
+        image: cacodemon,
         difficulty: 'easy',
         found: false,
       },
