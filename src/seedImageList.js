8d58161f0526f38ff6319bb332d6d31b43ac765e
jcLucio <c.lucio3@gmail.com>
2020-11-01 18:20:09 -0600

    Update imageList

--- src/seedImageList.js
+++ src/seedImageList.js
@@ -1,5 +1,11 @@
 const imageList = [
   {
+    /* 
+      The Loc Nar by Egor Klyuchnyk
+      Instagram https://www.instagram.com/ad.2.222/
+      Patreon https://www.patreon.com/Egor
+      Buy his poster https://anomaly-world.com/posters-by-anomaly-world/cyberpunk-poster-ad2222
+    */
     id: 'the-loc-nar',
     imageName: 'The Loc Nar',
     imageUrl: 'https://i.imgur.com/N4wefbX.jpg',
@@ -37,17 +43,22 @@ const imageList = [
     ],
   },
   {
-    id: 'second-image',
-    imageName: 'Second Image',
-    imageUrl: 'https://i.imgur.com/N4wefbX.jpg',
+    /* 
+      Universe 113 by Egor Klyuchnyk
+      Instagram https://www.instagram.com/ad.2.222/
+      Patreon https://www.patreon.com/Egor
+      Buy his poster https://anomaly-world.com/posters-by-anomaly-world/universe-113-poster
+    */
+    id: 'universe-113',
+    imageName: 'Universe 113',
+    imageUrl: 'https://i.redd.it/y2ljm914ovi51.jpg',
     itemList: [
       // Easy
       {
-        id: 'bowser',
-        name: 'Bowser',
-        franchise: 'Super Mario Bros',
-        image:
-          'https://upload.wikimedia.org/wikipedia/en/1/11/BowserNSMBUD.png',
+        id: 'cacodemon',
+        name: 'Cacodemon',
+        franchise: 'DOOM',
+        image: 'https://doomwiki.org/w/images/d/dd/Codex_cacodemon.bimage.png',
         difficulty: 'easy',
         found: false,
       },
