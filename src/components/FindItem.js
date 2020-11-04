9e7aa662dd8036da958aa7b9760c8247662949f5
jcLucio <c.lucio3@gmail.com>
2020-11-04 15:57:18 -0600

    Add conditional found style

--- src/components/FindItem.js
+++ src/components/FindItem.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './FindItem.module.scss';
 
-function FindItem({ name, franchise, image }) {
+function FindItem({ name, franchise, image, found }) {
   return (
-    <div className={styles.root}>
+    <div className={`${styles.root} ${found && styles.found}`}>
       <img className={styles['item-image']} src={image} alt={name} />
-      {/* conditional style w/found prop */}
       <div>
         <h3>{name}</h3>
         <h5>{franchise}</h5>
