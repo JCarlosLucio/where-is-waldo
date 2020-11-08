d1fc896edce4e7990954d4f64c07dca465d9e859
jcLucio <c.lucio3@gmail.com>
2020-11-07 20:56:18 -0600

    Add Button component

--- src/components/HighScoreDialog.js
+++ src/components/HighScoreDialog.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from './Button';
 import styles from './HighScoreDialog.module.scss';
 
 function HighScoreDialog() {
   return (
     <div className={styles.root}>
       <h1>High Scores</h1>
+      <Button>Submit</Button>
     </div>
   );
 }
