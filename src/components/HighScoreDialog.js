987fd3fc5446c5b05a98d65a527fda5a1c2bd244
jcLucio <c.lucio3@gmail.com>
2020-11-07 21:12:31 -0600

    Add High Scores strucuture

--- src/components/HighScoreDialog.js
+++ src/components/HighScoreDialog.js
@@ -5,6 +5,20 @@ import styles from './HighScoreDialog.module.scss';
 function HighScoreDialog() {
   return (
     <div className={styles.root}>
+      <div>
+        <h1>High Scores</h1>
+        <ul>
+          <li>
+            <p>1 Name 00:02:10</p>
+          </li>
+          <li>
+            <p>2 Name 00:02:11</p>
+          </li>
+          <li>
+            <p>3 Name 00:02:12</p>
+          </li>
+        </ul>
+      </div>
       <form>
         <label>
           Name
