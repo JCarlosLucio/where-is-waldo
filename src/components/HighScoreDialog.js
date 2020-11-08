168deef79d9b89e19cc16775f6507dadd2143906
jcLucio <c.lucio3@gmail.com>
2020-11-07 21:43:44 -0600

    Add styles for highscores

--- src/components/HighScoreDialog.js
+++ src/components/HighScoreDialog.js
@@ -5,12 +5,19 @@ import styles from './HighScoreDialog.module.scss';
 function HighScoreDialog() {
   return (
     <div className={styles.root}>
-      <div>
+      <div className={styles.highscores}>
         <h1>High Scores</h1>
         <ol>
           <li>Name 00:02:10</li>
           <li>Name 00:02:11</li>
           <li>Name 00:02:12</li>
+          <li>Name 00:02:13</li>
+          <li>Name 00:02:14</li>
+          <li>Name 00:02:15</li>
+          <li>Name 00:02:16</li>
+          <li>Name 00:02:17</li>
+          <li>Name 00:02:18</li>
+          <li>Name 00:02:19</li>
         </ol>
       </div>
       <form className={styles.form}>
