294a15f4faa1e853ad746bbc6ee862ff0933d640
jcLucio <c.lucio3@gmail.com>
2020-11-09 19:10:37 -0600

    Refactor to use useFirestore custom hook

--- src/components/HighScoreDialog.js
+++ src/components/HighScoreDialog.js
@@ -1,32 +1,16 @@
 import React from 'react';
+import useFirestore from '../hooks/useFirestore';
 import useInput from '../hooks/useInput';
 import Button from './Button';
 import { formatTime } from '../utils/index';
 import styles from './HighScoreDialog.module.scss';
 
-const seedScores = [
-  {
-    id: 'aaa-1',
-    name: 'aaa',
-    time: 1,
-  },
-  {
-    id: 'aab-2',
-    name: 'aab',
-    time: 2,
-  },
-  {
-    id: 'abb-3',
-    name: 'abb',
-    time: 3,
-  },
-];
-
 function HighScoreDialog({ timer }) {
   const [name, handleChange] = useInput('');
+  const [scores] = useFirestore('scores');
 
-  // using seedScores for testing
-  const scores = seedScores.map((score) => (
+  // use scores from firestore
+  const scoresList = scores.map((score) => (
     <li key={score.id}>
       {score.name.toUpperCase()} {formatTime(score.time)}
     </li>
@@ -36,7 +20,8 @@ function HighScoreDialog({ timer }) {
     <div className={styles.root}>
       <div className={styles.highscores}>
         <h1>High Scores</h1>
-        <ol className={styles.scores}>{scores}</ol>
+
+        <ol className={styles.scores}>{scoresList}</ol>
       </div>
       <form className={styles.form}>
         <h2>Add Your Score</h2>
