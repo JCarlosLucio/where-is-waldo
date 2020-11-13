492f412530446544a48a5dc37b8a4103e71d5a36
jcLucio <c.lucio3@gmail.com>
2020-11-13 15:11:07 -0600

    Update MadeBy to use link and icon props

--- src/components/MadeBy.js
+++ src/components/MadeBy.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './MadeBy.module.scss';
 
-function MadeBy({ position, children }) {
-  return <div className={`${styles.root} ${styles[position]}`}>{children}</div>;
+function MadeBy({ position, text, link, icon }) {
+  return (
+    <div className={`${styles.root} ${styles[position]}`}>
+      {text} <a href={link}>{icon}</a>
+    </div>
+  );
 }
 
 export default MadeBy;
