094df50ac44134c8b4ee8c7ddde36953684f5d11
jcLucio <c.lucio3@gmail.com>
2020-11-21 17:05:45 -0600

    Refactor onClick to not use relX0/Y0 from item

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -5,11 +5,11 @@ function ContextMenu({ list, xPos, yPos, handleMenuClick }) {
   const notFound = list.filter((item) => !item.found).map((item) => (
     <li
       key={item.id}
+      className={styles.item}
       onClick={(e) => {
         e.stopPropagation();
-        handleMenuClick(item.name, item.id, item.relX0, item.relY0, xPos, yPos);
+        handleMenuClick(item.name, item.id, xPos, yPos);
       }}
-      className={styles.item}
     >
       {item.name}
     </li>
