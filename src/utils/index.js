fdbbf0011474cef2c5a852fd832890c217d5f319
jcLucio <c.lucio3@gmail.com>
2020-11-22 18:45:37 -0600

    Update formatTime to handle seconds w/decimals

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export const formatTime = (time) => {
-  const getSeconds = `0${time % 60}`.slice(-2);
+  const getSeconds = `0${Math.round(time % 60)}`.slice(-2);
   const minutes = `${Math.floor(time / 60)}`;
   const getMinutes = `0${minutes % 60}`.slice(-2);
   const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
