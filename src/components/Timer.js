08fb3e643f1a8fb18b301daf01203cad6ab7fc8d
jcLucio <c.lucio3@gmail.com>
2020-11-22 22:26:55 -0600

    Add timer logic to work w/ isGameOver

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { formatTime } from '../utils';
 
-function Timer() {
-  return <Fragment>TIMER</Fragment>;
+function Timer({ isGameOver }) {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(
+    () => {
+      // starting/reseting of timer is controlled by isGameOver prop
+      // this way we avoid uncessary re-rendering other components when setting timer state
+      // (like I was doing before when timer state was in App component)
+      // or avoid having to call start/reset function from a parent
+      // https://stackoverflow.com/questions/37949981/call-child-method-from-parent
+
+      let interval;
+      if (!isGameOver) {
+        // start interval/timer
+        interval = setInterval(() => {
+          setTimer((timer) => timer + 1);
+        }, 1000);
+      } else if (isGameOver) {
+        // stops/resets timer
+        clearInterval(interval);
+        setTimer(0);
+      }
+
+      // when component unmounts stops timer / clearInterval
+      return () => {
+        clearInterval(interval);
+      };
+    },
+    [isGameOver]
+  );
+
+  return <Fragment>{formatTime(timer)}</Fragment>;
 }
 
 export default Timer;
