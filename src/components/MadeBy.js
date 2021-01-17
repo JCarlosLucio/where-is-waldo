8d7e1267942bf99395d2c2c23642f87bfdd011a7
jcLucio <c.lucio3@gmail.com>
2021-01-17 12:59:53 -0600

    Fix MadeBy Links not working

--- src/components/MadeBy.js
+++ src/components/MadeBy.js
@@ -2,10 +2,19 @@ import React from 'react';
 import styles from './MadeBy.module.scss';
 
 function MadeBy({ position, text, author, link }) {
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener = null;
+  };
+
   return (
     <div className={`${styles.root} ${styles[position]}`}>
       {text}{' '}
-      <a className={styles.author} href={link}>
+      <a
+        className={styles.author}
+        href={link}
+        onClick={() => openInNewTab(link)}
+      >
         {author}
       </a>
     </div>
